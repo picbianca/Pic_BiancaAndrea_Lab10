f08df895627309cde96e959b0a1f74959be54c24
BIANCA-ANDREA PIC <bianca.pic@stud.ubbcluj.ro>
2021-12-29 11:55:21 +0200

    Lab11

---
+++ Pic_BiancaAndrea_Lab10/Models/ListProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+namespace Pic_BiancaAndrea_Lab10.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
---
+++ Pic_BiancaAndrea_Lab10/ValidationBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace Pic_BiancaAndrea_Lab10
+{
+    class ValidationBehaviour : Behavior<Editor>
+    {
+
+        protected override void OnAttachedTo(Editor entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor =
+            string.IsNullOrEmpty(args.NewTextValue) ? Color.Red : Color.Default;
+        }
+    }
+}
